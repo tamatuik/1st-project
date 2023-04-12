942c1abf832c3ea77b2f093ae38defca97ca539b
Admin <Admin@DESKTOP-BRII8FF>
2023-04-12 19:25:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	cout << "\t\"I love C++!\n";
+	cout << "\t\tI love C++!\n";
+	cout << "\t\t\tI love C++!\"\n";
+	cout << "\t\t\t\tI love C++!\"\n";
+	cout << "\t\t\t\t\tI love C++!\"\n";
+}
